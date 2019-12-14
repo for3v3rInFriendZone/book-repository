e3b25f5aceb6ec2817d80797fd1e8fbbd416b06c
for3v3rInFriendZone <strile93@gmail.com>
2019-12-14 18:51:23 +0100

    Changed Book model

--- src/main/java/com/repository/books/model/Book.java
+++ src/main/java/com/repository/books/model/Book.java
@@ -22,6 +22,6 @@ public class Book implements Serializable {
   private String publicationLanguage;
   private String form;
   private String keepingPlace;
-  private List<Category> categories;
+  private List<String> categories;
   private Long inventoryNumber;
 }
