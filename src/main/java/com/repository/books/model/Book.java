73c0ec66baa8d34006adebce93ad82fa4d381985
for3v3rInFriendZone <strile93@gmail.com>
2020-04-17 22:44:07 +0200

    Saving authors as a string, not as a list of strings.

--- src/main/java/com/repository/books/model/Book.java
+++ src/main/java/com/repository/books/model/Book.java
@@ -4,27 +4,26 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 @Setter
 public class Book implements Serializable {
 
-  private static final long serialVersionUID = 5703523454260669753L;
+    private static final long serialVersionUID = 5703523454260669753L;
 
-  private String id;
-  private String title;
-  private List<String> authors;
-  private String publisher;
-  private Integer publishedYear;
-  private String publication;
-  private Integer numberOfPages;
-  private String publicationLanguage;
-  private String form;
-  private String keepingPlace;
-  private List<String> categories;
-  private Long inventoryNumber;
-  private String image;
-  private String createdAt;
+    private String id;
+    private String title;
+    private String authors;
+    private String publisher;
+    private Integer publishedYear;
+    private String publication;
+    private Integer numberOfPages;
+    private String publicationLanguage;
+    private String form;
+    private String keepingPlace;
+    private List<String> categories;
+    private Long inventoryNumber;
+    private String image;
+    private String createdAt;
 }
